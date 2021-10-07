0ef1a5fc94f80378640b8883da83142160d07b56
hetvigudka28 <hetvigudka28@gmail.com>
2021-10-08 00:10:00 +0530

    Products and Orders Model

--- CaretexBackend/webapp/admin.py
+++ CaretexBackend/webapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
+from .models import Products, AllOrders
 
 # Register your models here.
+admin.site.register(Products)
+admin.site.register(AllOrders)
--- CaretexBackend/webapp/models.py
+++ CaretexBackend/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Products(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    GTIN=models.BigIntegerField()
+    code=models.CharField(max_length=25)
+    name=models.CharField(max_length=100)
+    description=models.TextField(max_length=700)
+    mrp=models.FloatField()
+    quantity=models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class AllOrders(models.Model):
+    amount=models.FloatField()
+    product=models.ManyToManyField(Products)
